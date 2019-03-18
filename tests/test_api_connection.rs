10ea2b1afbe207be000e159c1a7184797ab18230
italic <super3dblender@yahoo.com>
2019-03-17 19:36:03 -0700

    Tests: document test module. Provide useful panic messages.

--- tests/test_api_connection.rs
+++ tests/test_api_connection.rs
@@ -1,3 +1,14 @@
+//! SDRplay devices retrieve data from an RF receiver and returns data based on
+//! its USB interface, device type, RF reception, etc. Most tests will simply
+//! print data, since a lot of data (ie serial number, device name) is unique
+//! and not all user devices will be equivalent to the test device. The device
+//! used to build this wrapper and associated tests is the RSPduo dual-tuner
+//! model. The driver used is provided by SDRplay, version 2.13.
+//!
+//! Serial number: 1809003232
+//! Device name: SDRplay_RSPII_VID_1DF7&PID_3020_BUS_001_PORT_002
+//! Hardware version: 3
+
 #![allow(unused_imports)]
 
 use std::ffi::CStr;
@@ -11,11 +22,12 @@ fn api_version() -> () {
     unsafe {
         mir_sdr_ApiVersion(&mut ver);
     }
-    assert_eq!(ver, 2.13);
-    println!("API version: {}", ver);
+    assert_eq!(
+        ver, 2.13,
+        "API version not supported. Please install version 2.13 from SDRplay."
+    );
 }
 
-/// Because each device is different, only print device data.
 #[test]
 fn get_devices() {
     let max_devices: c_uint = 4;  // arbitrary limit
@@ -42,21 +54,20 @@ fn get_devices() {
     };
     assert!(err_return == mir_sdr_ErrT_mir_sdr_Success);
     devices.truncate(num_dev as usize);
-
-    // TEST DEVICE:
-    // SerNo: 1809003232
-    // DevNm: SDRplay_RSPII_VID_1DF7&PID_3020_BUS_001_PORT_002
-    // hwVer: 3
+    assert!(
+        devices.len() > 0,
+        "Did not find devices. Please ensure device is connected."
+    );
 
     for dev in &devices {
-        println!("device: {:?}", &dev);
+        println!("Device: {:?}", &dev);
 
         let ser_no = unsafe {CStr::from_ptr(dev.SerNo)};
         let ser_no = ser_no.clone().to_string_lossy().parse::<i32>();
         if let Ok(ser) = ser_no {
             println!("SerNo: {}", &ser);
         } else {
-            panic!("Could not retrieve device serial number.");
+            panic!("Could not parse device serial number.");
         }
 
         let dev_nm = unsafe {CStr::from_ptr(dev.DevNm)}.clone().to_string_lossy();

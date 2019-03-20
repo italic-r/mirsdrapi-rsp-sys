8a75a134b0d0d57056b8ca81f87abd7af88a8a5f
italic <super3dblender@yahoo.com>
2019-03-19 21:15:19 -0700

    Tests: add stubs for other API control functions. These are generally in the order they are declared in the original header, with deprecated functions listed first.

--- tests/test_api_connection.rs
+++ tests/test_api_connection.rs
@@ -17,6 +17,77 @@ use std::os::raw::{c_uchar, c_char, c_uint, c_int};
 use mirsdrapi_rsp_sys::*;
 
 
+/* Deprecated API functions */
+
+#[test]
+#[ignore]
+fn init() {
+    assert!(true);
+}
+
+#[test]
+#[ignore]
+fn uninit() {
+    assert!(true);
+}
+
+#[test]
+#[ignore]
+fn read_packet() {
+    assert!(true);
+}
+
+#[test]
+#[ignore]
+fn set_param() {
+    assert!(true);
+}
+
+#[test]
+#[ignore]
+fn get_gr_by_freq() {
+    assert!(true);
+}
+
+/* END Deprecated API functions */
+
+
+#[test]
+fn reinit() {
+}
+
+#[test]
+fn set_rf() {
+}
+
+#[test]
+fn set_fs() {
+}
+
+#[test]
+fn set_gr() {
+}
+
+#[test]
+fn set_gr_params() {
+}
+
+#[test]
+fn set_dc_mode() {
+}
+
+#[test]
+fn set_dc_track_time() {
+}
+
+#[test]
+fn set_sync_update_sample_num() {
+}
+
+#[test]
+fn set_sync_update_period() {
+}
+
 #[test]
 fn api_version() -> () {
     let mut ver: f32 = 0.;
@@ -27,6 +98,62 @@ fn api_version() -> () {
         ver, 2.13,
         "API version not supported. Please install version 2.13 from SDRplay."
     );
+}
+
+#[test]
+fn reset_update_flags() {
+}
+
+#[test]
+fn set_transfer_mode() {
+}
+
+#[test]
+fn down_convert() {
+}
+
+#[test]
+fn set_ppm() {
+}
+
+#[test]
+fn set_lo_mode() {
+}
+
+#[test]
+fn set_gr_alt_mode() {
+}
+
+#[test]
+fn dc_offset_iq_imbalance_control() {
+}
+
+#[test]
+fn decimate_control() {
+}
+
+#[test]
+fn agc_control() {
+}
+
+#[test]
+fn stream_init() {
+}
+
+#[test]
+fn stream_uninit() {
+}
+
+#[test]
+fn debug_enable() {
+}
+
+#[test]
+fn get_current_gain() {
+}
+
+#[test]
+fn gain_change_callback_message_received() {
 }
 
 fn _get_devices() -> Result<Vec<mir_sdr_DeviceT>, &'static str> {
@@ -86,4 +213,89 @@ fn get_devices() {
     } else {
         panic!("Test \"get_devices()\" failed.")
     }
+}
+
+#[test]
+fn set_device_idx() {
+}
+
+#[test]
+fn release_device_idx() {
+}
+
+#[test]
+fn get_hw_version() {
+}
+
+
+/* RSP2 */
+
+#[test]
+fn rsp2_antenna_control() {
+}
+
+#[test]
+fn rsp2_external_reference_control() {
+}
+
+#[test]
+fn rsp2_biasT_control() {
+}
+
+#[test]
+fn rsp2_rf_notch_enable() {
+}
+
+#[test]
+fn rsp_set_gr() {
+}
+
+#[test]
+fn rsp_set_gr_limits() {
+}
+
+#[test]
+fn am_port_select() {
+}
+
+
+/* RSP 1a */
+
+#[test]
+fn rsp1a_biastT() {
+}
+
+#[test]
+fn rsp1a_dab_notch() {
+}
+
+#[test]
+fn rsp1a_broadcast_notch() {
+}
+
+
+/* RSPduo */
+
+#[test]
+fn rspduo_tuner_select() {
+}
+
+#[test]
+fn rspduo_ext_ref() {
+}
+
+#[test]
+fn rspduo_biasT() {
+}
+
+#[test]
+fn rspduo_tuner1_am_notch() {
+}
+
+#[test]
+fn rspduo_broadcast_notch() {
+}
+
+#[test]
+fn rspduo_dab_notch() {
 }

146b2661b7963b18655c2263b7bd8de42ffbcd6f
italic <super3dblender@yahoo.com>
2019-07-30 22:49:21 -0700

    Default::default() for mir_sdr_DeviceT
    This makes it easier to create dummy device objects for init in safe wrapper.

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,14 @@
 #![allow(non_snake_case)]
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+impl Default for mir_sdr_DeviceT {
+    fn default() -> Self {
+        mir_sdr_DeviceT {
+            SerNo: &mut 0,
+            DevNm: &mut 0,
+            hwVer: 0,
+            devAvail: 1,
+        }
+    }
+}
